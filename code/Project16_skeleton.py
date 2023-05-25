7d7b9dbc404d689eb5d394e999cf932b1b08c519
manonvictoria <126787846+manonvictoria@users.noreply.github.com>
2023-05-25 19:31:00 +0200

    Update Project16_skeleton.py

--- code/Project16_skeleton.py
+++ code/Project16_skeleton.py
@@ -271,3 +271,360 @@ plt.savefig('../output/physical_health.jpg')
 plt.tight_layout()
 #plt.show()
 '''
+
+
+
+
+print("##################################################################Logistic Regression & Random Forest##################################################################")
+
+# Import packages that are needed
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, confusion_matrix, auc
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+# Utility function to plot the diagonal line
+def add_identity(axes, *line_args, **line_kwargs):
+    identity, = axes.plot([], [], *line_args, **line_kwargs)
+    def callback(axes):
+        low_x, high_x = axes.get_xlim()
+        low_y, high_y = axes.get_ylim()
+        low = max(low_x, low_y)
+        high = min(high_x, high_y)
+        identity.set_data([low, high], [low, high])
+    callback(axes)
+    axes.callbacks.connect('xlim_changed', callback)
+    axes.callbacks.connect('ylim_changed', callback)
+    return axes
+
+# Functions you are asked to complete
+def get_confusion_matrix(y, y_pred):
+    """
+    compute the confusion matrix of a classifier yielding
+    predictions y_pred for the true class labels y
+    :param y: true class labels
+    :type y: numpy array
+
+    :param y_pred: predicted class labels
+    :type y_pred: numpy array
+
+    :return: comfusion matrix comprising the
+             true positives (tp),
+             true negatives  (tn),
+             false positives (fp),
+             and false negatives (fn)
+    :rtype: four integers
+    """
+
+    # true/false pos/neg.
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+
+    # define positive and negative classes.
+    ''''
+    for i in range (len(y)):
+        if y[i] == 'Yes' and y_pred[i] == 'Yes':
+            tp += 1
+        elif y[i] == 'No' and y_pred[i] == 'Yes':
+            fp += 1
+        elif y[i] == 'No' and y_pred[i] == 'No':
+            tn += 1
+        elif y[i] == 'Yes' and y_pred[i] == 'No':
+            fn += 1
+
+    return tn, fp, fn, tp
+    '''
+
+    for i in range (len(y)):
+        if y[i] == 1 and y_pred[i] == 1:
+            tp += 1
+        elif y[i] == 0 and y_pred[i] == 1:
+            fp += 1
+        elif y[i] == 0 and y_pred[i] == 0:
+            tn += 1
+        elif y[i] == 1 and y_pred[i] == 0:
+            fn += 1
+
+
+    '''
+    for i in range (0, len(y)):
+        if y[i, 1] == 1 and y_pred[i] == 1:
+            tp += 1
+        elif y[i, 1] == 0 and y_pred[i] == 1:
+            fp += 1
+        elif y[i, 1] == 0 and y_pred[i] == 0:
+            tn += 1
+        elif y[i, 1] == 1 and y_pred[i] == 0:
+            fn += 1
+    '''
+    print(tn)
+
+    return tn, fp, fn, tp
+
+def evaluation_metrics(clf, y, X, ax,legend_entry='my legendEntry'):
+    """
+    compute multiple evaluation metrics for the provided classifier given the true labels
+    and input features. Provides a plot of the roc curve on the given axis with the legend
+    entry for this plot being specified, too.
+
+    :param clf: true class labels
+    :type clf: numpy array
+
+    :param y: true class labels
+    :type y: numpy array
+
+    :param X: feature matrix
+    :type X: numpy array
+
+    :param ax: matplotlib axis to plot on
+    :type legend_entry: matplotlib Axes
+
+    :param legend_entry: the legend entry that should be displayed on the plot
+    :type legend_entry: string
+
+    :return: comfusion matrix comprising the
+             true positives (tp),
+             true negatives  (tn),
+             false positives (fp),
+             and false negatives (fn)
+    :rtype: four integers
+    """
+
+    # Get the label predictions
+    y_test_pred = clf.predict(X)
+    
+
+    #convert y into an array-> same dimension as y_test_pred (=> needed for def get_confusion_matrix)
+    y = y.values
+
+
+    print(len(y))
+    print(y)
+    print(y.shape)
+    print(len(y_test_pred))
+    print(y_test_pred)
+
+    # Calculate the confusion matrix given the predicted and true labels with your function
+    tn, fp, fn, tp = get_confusion_matrix(y, y_test_pred)
+
+
+    # Ensure that you get correct values - this code will divert to
+    # sklearn if your implementation fails - you can ignore those lines
+    tn_sk, fp_sk, fn_sk, tp_sk = confusion_matrix(y, y_test_pred).ravel()
+    if np.sum([np.abs(tp-tp_sk) + np.abs(tn-tn_sk) + np.abs(fp-fp_sk) + np.abs(fn-fn_sk)]) >0:
+        print('OWN confusion matrix failed!!! Reverting to sklearn.')
+        tn = tn_sk
+        tp = tp_sk
+        fn = fn_sk
+        fp = fp_sk
+    else:
+        print(':) Successfully implemented the confusion matrix!')
+
+    # Calculate the evaluation metrics
+    precision   = tp / (tp + fp)
+    specificity = tn / (tn + fp)
+    accuracy    = (tp + tn) / (tp + fp + tn + fn)
+    recall      = tp / (tp + fn)
+    f1          = tp/(tp + 0.5*(fp+fn))
+
+    # Get the roc curve using a sklearn function
+    y_test_predict_proba  = clf.predict_proba(X)[:, 1]
+    fp_rates, tp_rates, _ = roc_curve(y, y_test_predict_proba)
+
+    # Calculate the area under the roc curve using a sklearn function
+    roc_auc = auc(fp_rates, tp_rates)
+
+    # Plot on the provided axis
+    ax.plot(fp_rates, tp_rates,label = legend_entry)
+
+
+    return [accuracy, precision, recall, specificity, f1, roc_auc]
+
+#Import data
+#data = pd.read_csv("./data/heart_2020_cleaned.csv")
+
+data_encoded['HeartDisease'] = data_encoded['HeartDisease'].replace({'Yes': 1, 'No': 0}) #-> already converted into boolean BUT (True/Fasle)
+X  = data_encoded.copy().drop('HeartDisease', axis = 1)
+y  = data_encoded['HeartDisease']
+
+#print(X)
+#print(y)
+
+
+# Perform a 5-fold crossvalidation - prepare the splits
+n_splits = 5
+skf      = StratifiedKFold(n_splits=n_splits)
+
+
+# Prepare the performance overview data frame
+data_encoded_performance = pd.DataFrame(columns = ['fold','clf','accuracy','precision','recall','specificity','F1','roc_auc'])
+data_encoded_LR_normcoef = pd.DataFrame(index = X.columns, columns = np.arange(n_splits))
+
+# Use this counter to save your performance metrics for each crossvalidation fold
+# also plot the roc curve for each model and fold into a joint subplot
+fold = 0
+fig,axs = plt.subplots(1,2, figsize=(9, 4))
+# with KNN: fig,axs = plt.subplots(1,3, figsize=(9, 4))
+
+
+
+# Loop over all splits
+for train_index, test_index in skf.split(X, y):
+
+    # Get the relevant subsets for training and testing
+    X_test  = X.iloc[test_index]
+    y_test  = y.iloc[test_index]
+    X_train = X.iloc[train_index]
+    y_train = y.iloc[train_index]
+
+    #print('X_test', X_test, 'y_test', y_test, 'X_train', X_train, 'y_train', y_train)
+    
+
+    # Standardize the numerical features using training set statistics
+    sc = StandardScaler()
+    X_train_sc = sc.fit_transform(X_train)
+    X_test_sc  = sc.transform(X_test)
+
+    #print('X_train_sc', X_train_sc)
+    #print('X_test_sc', X_test_sc)
+
+    # Creat prediction models and fit them to the training data
+
+    # Logistic regression
+    LR_clf = LogisticRegression(random_state=1)
+    LR_clf.fit(X_train_sc, y_train)
+
+    #print('LR_clf', clf)
+
+
+    # Get the top 5 features that contribute most to the classification
+    data_encoded_this_LR_coefs = pd.DataFrame(zip(X_train.columns, np.transpose(LR_clf.coef_[0])), columns=['features', 'coef'])
+    data_encoded_LR_normcoef[data_encoded_this_LR_coefs['features']] = data_encoded_this_LR_coefs['coef'].values / data_encoded_this_LR_coefs['coef'].abs().sum()
+    #data_encoded_LR_normcoef.loc[:,fold] = data_encoded_this_LR_coefs['coef'].values/data_encoded_this_LR_coefs['coef'].abs().sum()
+    #data_encoded_this_LR_coefs = pd.DataFrame(zip(X_train.columns, np.transpose(clf.coef_)), columns=['features', 'coef'])
+    #data_encoded_LR_normcoef.loc[:,fold] = data_encoded_this_LR_coefs['coef'].values
+
+
+
+    #print('top 5 features', data_encoded_this_LR_coefs, data_encoded_LR_normcoef)
+
+    # Random forest
+    RF_clf = RandomForestClassifier(random_state=1)
+    RF_clf.fit(X_train_sc, y_train)
+
+    #print('RF_clf', RF_clf)
+
+ 
+    #KNN = 
+
+
+
+
+
+
+
+
+    # Evaluate the classifiers Logistic Regression, Random Forest and KNN
+    eval_metrics = evaluation_metrics(LR_clf, y_test, X_test_sc, axs[0], legend_entry=str(fold))
+    data_encoded_performance.loc[len(data_encoded_performance)-1,:] = [fold,'LR']+eval_metrics
+
+    eval_metrics_RF = evaluation_metrics(RF_clf, y_test, X_test_sc, axs[1], legend_entry=str(fold))
+    data_encoded_performance.loc[len(data_encoded_performance)-1, :] = [fold, 'RF'] + eval_metrics_RF
+
+    print('eval metrics LR', eval_metrics)
+    
+    print('eval metrics RF', eval_metrics_RF)
+
+    '''
+    eval_metrics_KNN = evaluation_metrics(KNN_clf, y_test, X_test_sc, axs[2], legend_entry=str(fold))
+    data_encoded_performance.loc[len(data_encoded_performance)-1, :] = [fold, 'RF'] + eval_metrics_RF
+    '''
+
+    # increase counter for folds
+    fold += 1
+
+''''
+    print(data_encoded_performance.loc[len(data_encoded_performance)-1,:])
+    print(data_encoded_performance.loc[len(data_encoded_performance)-1, :])
+'''
+
+model_names = ['LR','RF'] #add 'KNN'
+for i,ax in enumerate(axs):
+    ax.set_xlabel('FPR')
+    ax.set_ylabel('TPR')
+    add_identity(ax, color="r", ls="--",label = 'random\nclassifier')
+    ax.legend()
+    ax.title.set_text(model_names[i])
+plt.tight_layout()
+plt.savefig('./output/roc_curves.png')
+
+
+# Summarize the folds
+print(data_encoded_performance.groupby(by = 'LR_clf').mean())
+print(data_encoded_performance.groupby(by = 'LR_clf').std())
+
+
+# Get the top features - evaluate the coefficients across the five folds
+data_encoded_LR_normcoef['importance_mean'] = data_encoded_LR_normcoef.mean(axis =1)
+data_encoded_LR_normcoef['importance_std']  = data_encoded_LR_normcoef.std(axis =1)
+data_encoded_LR_normcoef['importance_abs_mean'] = data_encoded_LR_normcoef.abs().mean(axis =1)
+data_encoded_LR_normcoef.sort_values('importance_abs_mean', inplace = True, ascending=False)
+
+# Visualize the normalized feature importance across the five folds and add error bar to indicate the std
+fig, ax = plt.subplots(figsize=(9, 6))
+ax.bar(np.arange(15), data_encoded_LR_normcoef['importance_abs_mean'][:15], yerr=data_encoded_LR_normcoef['importance_std'][:15])
+#ax.set_xticklabels(data_encoded_LR_normcoef.index.tolist()[:15], rotation=90)
+ax.set_xticks(np.arange(15), data_encoded_LR_normcoef.index.tolist()[:15], rotation=90)
+ax.set_title("Normalized feature importance for LR across 5 folds", fontsize=20)
+plt.xlabel('Features', fontsize=16)
+plt.ylabel("Normalized feature importance", fontsize=16)
+plt.tight_layout()
+plt.savefig('./output/importance.png')
+
+# Get the two most important features and the relevant sign:
+data_encoded_LR_normcoef.index[:2]
+data_encoded_LR_normcoef['importance_mean'][:2]
+
+
+print ("****************** Zusatz Annika ***************************")
+# Get the coefficients for all features
+coefficients = pd.DataFrame(zip(X_train.columns, np.transpose(LR_clf.coef_[0])), columns=['features', 'coef'])
+coefficients = coefficients.sort_values(by='coef', ascending=False)
+
+# Print all features and their coefficients
+print(coefficients)
+coefficients.to_csv('./output/table_with_coefficients.csv', index=False)
+'''
+positive Koeffizienten: höheres Risiko für Herzkrankheiten
+- Alterskategorien: höchsten positiven Koeffizienten --> höheres Alter = erhöhtes Risiko für Herzkrankheiten verbunden (Koeffizienten nehmen mit steigendem Alter ab --> Risiko mit höherem Alter nimmt geringfügig ab)
+- Allgemeine Gesundheit (GenHealth) --> Kategorien "Good" und "Fair" einen höheren Einfluss haben als "Poor" und "Very good" --> schlechtere allgemeine Gesundheit = höheres Risiko für Herzkrankheiten 
+- Geschlecht (Sex_Male) --> Männer haben ein etwas höheres Risiko für Herzkrankheiten als Frauen
+- Schlaganfall (Stroke_Yes), Rauchen (Smoking_Yes), Diabetes (Diabetic_Yes), Nierenerkrankungen (KidneyDisease_Yes) und Asthma (Asthma_Yes) --> erhöhtes Risiko für Herzkrankheiten 
+- BMI, geistige Gesundheit (MentalHealth), körperliche Gesundheit (PhysicalHealth) geringere positive Koeffizienten --> geringeren Einfluss auf das Herzkrankheitsrisiko 
+Negative Koeffizeinten: geringeren Risiko für Herzkrankheiten 
+- Rassenzugehörigkeit (Race)  
+- Hautkrebs (SkinCancer_Yes) 
+'''
+data_encoded_performance.to_csv('./output/table_with_performance.csv', index=False)
+'''
+- Accuracy: Prozentsatz der korrekt vorhergesagten Werte insgesamt --> LR-Classifier durchschnittliche Accuracy über die Folds: 0.916, RF-Classifier: 0.905 l --> LR-Classifier weist tendenziell eine etwas höhere Gesamtgenauigkeit auf
+- Precision: Anteil der korrekt positiv vorhergesagten Werte (true positives) unter allen positiven Vorhersagen --> LR-Classifier durchschnittliche Precision: 0.544, RF-Classifier: 0.341 aufweist --> LR-Classifier tendenziell eine bessere Fähigkeit hat, echte positive Fälle zu identifizieren
+- Recall (Sensitivität): Anteil der korrekt positiv vorhergesagten Werte (true positives) unter allen tatsächlich positiven Fällen --> LR-Classifier: 0.107, RF-Classifier: 0.120 --> RF-Classifier hat tendenziell eine etwas bessere Fähigkeit hat, tatsächlich positive Fälle zu erkennen
+- Specificity: Anteil der korrekt negativ vorhergesagten Werte (true negatives) unter allen tatsächlich negativen Fällen an --> LR-Classifier: 0.992, RF-Classifier: 0.978 --> LR-Classifier hat eine höhere Fähigkeit, echte negative Fälle zu identifizieren
+- F1-Score: harmonische Mittel aus Precision und Recall und gibt einen kombinierten Maßstab für die Modellleistung --> LR-Classifier durchschnittlicher F1-Score: 0.178, RF-Classifier: 0.178 --> ähnliche F1-Scores
+- ROC-AUC: Fähigkeit des Modells beschreibt, zwischen positiven und negativen Fällen zu unterscheiden --> LR-Classifier durchschnittliche ROC-AUC: 0.840, RF-Classifier: 0.787 --> LR-Classifier tendenziell eine bessere Fähigkeit, zwischen positiven und negativen Fällen zu unterscheiden
+'''
+
+
+
+
+
+
+
